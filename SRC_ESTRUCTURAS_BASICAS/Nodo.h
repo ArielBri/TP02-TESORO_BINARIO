7727fd7c6f2afc70f28668b35631289ccf6c0b0b
JoaquinCladera <116321077+JoaquinCladera@users.noreply.github.com>
2023-10-16 20:13:50 -0300

    Lista

---
+++ SRC_ESTRUCTURAS_BASICAS/Nodo.h
@@ -0,0 +1,23 @@
+//
+//
+
+#ifndef NODO_H
+#define NODO_H
+
+template<class T>
+class Nodo {
+private:
+    T dato;
+    Nodo<T>* siguiente;
+
+public:
+    Nodo(T valor);
+    bool haySiguiente();
+    Nodo<T>* getSiguiente();
+    void setSiguiente(Nodo<T>* nuevoSig);
+    T getValor();
+    void setValor(T nuevoValor);
+    ~Nodo();
+};
+
+#endif
