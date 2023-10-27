973b0e46a6dbf13d61697c8addecbbe0d766fb12
JoaquinCladera <116321077+JoaquinCladera@users.noreply.github.com>
2023-10-27 09:15:05 -0300

    Update Ficha.h

--- SRC_ESTRUCTURAS_ORDENADAS/Ficha/Ficha.h
+++ SRC_ESTRUCTURAS_ORDENADAS/Ficha/Ficha.h
@@ -1,10 +1,38 @@
-#ifndef TP_2_FICHA_H
-#define TP_2_FICHA_H
+#ifndef FICHA_H_
+#define FICHA_H_
 
-
-class Ficha {
-
+enum TipoDeFicha{
+    TESORO,
+    ESPIA,
+    MINA
 };
 
+class Ficha {
+private:
+    TipoDeFicha ficha;
+    Jugador* jugador;
+    
+public:
+    /**
+	 * pre: se le pasa un tipo de ficha y un puntero a jugador para crear, cuando se da de alta el mapa ya se crea con tipo BLANCO y jugador en NULL
+	 * pos: crea una ficha
+	 */
+    Ficha(TipoDeFicha ficha, Jugador* jugador);
 
-#endif //TP_2_FICHA_H
+    /**
+	 * pos: elimina la ficha
+	 */
+    virtual ~Ficha();
+
+    /**
+	 * pos: devuelve el tipo de ficha como enum 
+	 */
+    TipoDeFicha getFicha();
+
+    /**
+	 * pos: Devuelve el jugador
+	 */
+    Jugador* getJugador();
+};
+
+#endif /* FICHA_H_ */
