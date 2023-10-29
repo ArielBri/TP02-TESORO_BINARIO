19877bb155835b6822a4387c18b855502ba4e43e
IvanFuschetto <133823780+IvanFuschetto@users.noreply.github.com>
2023-10-29 11:48:58 -0300

    Update Jugador.cpp

--- SRC_ESTRUCTURAS/Jugador.cpp
+++ SRC_ESTRUCTURAS/Jugador.cpp
@@ -1,20 +1,32 @@
 #include "Jugador.h"
-
-Jugador::Jugador() {
-
+#include <iostream>
+Jugador::Jugador(int numeroDeJugador)
+{
+    this->idJugador=numeroDeJugador;
+    this->cantCartasDelJugador=0;
+    this->cartasDelJugador= new Lista<Carta*>();
 }
-
-
-void Jugador::obtenerCarta(Mazo* mazo){
-    this->cartasJugador->add(mazo->obtenerCarta());
-}
-
-void Jugador::jugarCarta(Carta* carta) {
-    carta->usarCarta();
-}
-
-
-
 Jugador::~Jugador(){
-    delete cartasJugador;
+    delete []this->cartasDelJugador;
 }
+int Jugador::mostrarJugador(){
+    return this->idJugador;
+}
+void Jugador::obtenerCarta(Mazo* mazo){
+    this->cartasDelJugador->add(mazo->obtenerCarta());
+}
+void Jugador::mostrarCartas(){
+    int posicion=0;
+    this->cartasDelJugador->reiniciarCursor();
+    while(this->cartasDelJugador->avanzarCursor()){
+        Carta* carta= this->cartasDelJugador->getCursor();
+        //cartaAMostrar= carta->mostrarCarta();
+        std::cout<<"posicion - Carta"<<std::endl;
+        posicion++;
+    }
+}
+void Jugador::jugarCarta(int posicion){
+    Carta* carta= this->cartasDelJugador->get(posicion);
+    carta->usarCarta();
+    this->cartasDelJugador->remover(posicion);
+    }
