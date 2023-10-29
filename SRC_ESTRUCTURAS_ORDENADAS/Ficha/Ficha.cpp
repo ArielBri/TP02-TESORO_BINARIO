9dce51d91456e0c7d589f65cbc35af17466c716d
NicolasRosales <41493327+NicolasRosales@users.noreply.github.com>
2023-10-29 13:36:50 -0300

    Update Ficha.cpp

--- SRC_ESTRUCTURAS_ORDENADAS/Ficha/Ficha.cpp
+++ SRC_ESTRUCTURAS_ORDENADAS/Ficha/Ficha.cpp
@@ -1,18 +1,43 @@
+/*
+ * Ficha.cpp
+ *
+ *  Created on: 12/09/2023
+ *      Author: algo2
+ */
+
 #include "Ficha.h"
 
-Ficha::Ficha(TipoDeFicha ficha, Jugador* jugador){
-  this->ficha = ficha;
-  this->jugador = jugador;
+Ficha::Ficha(TipoFicha tipo, Jugador* jugador){
+	this->tipo= tipo;
+	this->jugador= jugador;
+	this->turnosRestantesBlindaje=0;
+}
+
+TipoFicha Ficha::getTipo(){
+	return this->tipo;
+}
+
+void Ficha::setTipo(TipoFicha tipo){
+	this->tipo= tipo;
+}
+Jugador* Ficha::getJugador(){
+	return this->jugador;
+}
+void Ficha::setJugador(Jugador* jugador){
+	this->jugador= jugador;
+}
+void Ficha::blindarTesoro(){
+	this->tipo= TesoroBlindado;
+	this->turnosRestantesBlindaje= 5;
+}
+
+void Ficha::restarContadorBlindaje(){
+	this->turnosRestantesBlindaje -=1;
+	if(this->turnosRestantesBlindaje== 0){
+		this->tipo= Tesoro;
+	}
 }
 
 Ficha::~Ficha() {
-	// TODO Auto-generated destructor stub
-}
 
-TipoDeFicha Ficha::getFicha(){
-  return this->ficha;
-}
-
-Jugador* Ficha::getJugador(){
-  return this->jugador;
 }
