bcd3d2599aece474feb0eb1888952374fd36bb94
carlicastro <105254519+carlicastro@users.noreply.github.com>
2023-11-07 19:00:41 -0300

    Add files via upload

---
+++ TP2-Modificado/Mazo/Carta.cpp
@@ -0,0 +1,57 @@
+#include "Carta.h"
+
+Carta::Carta(TipoCarta tipo) {
+    this->tipo = tipo;
+}
+void Carta::usarCarta() {
+    switch (this->tipo) {
+        case Blindaje:
+            jugarBlindaje();
+            break;
+        case Radar:
+            jugarRadar();
+            break;
+        case PartirTesoro:
+            partirTesoro();
+            break;
+        case Espias2:
+            jugarEspias2();
+            break;
+        case Bloqueo:
+            jugarBloqueo();
+            break;
+        case Cartas2:
+            jugarCartas2();
+            break;
+        default:
+            throw ("No se encontró una carta");
+    }
+}
+
+
+TipoCarta Carta::verCarta(){
+    return this->tipo;
+}
+
+
+void jugarBlindaje(){
+}
+void jugarRadar(){
+
+}
+void partirTesoro(){
+
+}
+void jugarEspias2(){
+
+}
+void jugarBloqueo(){
+
+}
+void jugarCartas2(){
+
+}
+
+
+Carta::~Carta() {
+}
