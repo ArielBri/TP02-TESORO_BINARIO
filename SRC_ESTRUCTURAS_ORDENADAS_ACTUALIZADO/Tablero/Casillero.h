4dc09c52e815216d96031c9c55dfe4a3f70b5b65
JoaquinCladera <116321077+JoaquinCladera@users.noreply.github.com>
2023-11-07 14:02:30 -0300

    Add files via upload

---
+++ SRC_ESTRUCTURAS_ORDENADAS_ACTUALIZADO/Tablero/Casillero.h
@@ -0,0 +1,48 @@
+#ifndef CASILLERO_H_
+#define CASILLERO_H_
+#include "../Ficha/Ficha.h"
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+enum EstadoCasillero{
+    Ocupado,
+    Vacio,
+    Inactivo
+};
+
+class Casillero {
+
+private:
+    EstadoCasillero estado;
+    Ficha* ficha;
+    int turnosRestantesInactivo;
+public:
+    /**
+     * pre: -
+     * pos: deja un casillero vacio creado de las coordenadas dadas
+     */
+    Casillero();
+
+    EstadoCasillero getEstado();
+    void setEstado(EstadoCasillero estado);
+
+    Ficha* getFicha();
+    void setFicha(Ficha* ficha);
+
+
+    void restarTurnoInactivo();
+    void ponerCasilleroInactivo();
+    /**
+     * pre: -
+     * pos: elimina el casillero
+     */
+
+
+    virtual ~Casillero();
+};
+
+
+
+#endif /* CASILLERO_H_ */
