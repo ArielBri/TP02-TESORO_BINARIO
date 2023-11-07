23109660bac4a19be83ee4aab27e7c60cafc48e6
carlicastro <105254519+carlicastro@users.noreply.github.com>
2023-11-07 03:37:11 -0300

    Update Jugador.h
    Declaracion devolverCantidadDeTesoros() y cantidadDeTesoros

--- SRC_ESTRUCTURAS_ORDENADAS_ACTUALIZADO/Jugador/Jugador.h
+++ SRC_ESTRUCTURAS_ORDENADAS_ACTUALIZADO/Jugador/Jugador.h
@@ -8,10 +8,25 @@
 class Jugador{
 private:
     int idJugador;
+    int cantidadDeTesoros;
     Lista<Carta*>* cartasDelJugador;
 public:
+	/*
+	 * Pre: numero de jugador y cantidad de tesoros debe ser entero mayor a 0
+	 * Pos: crea una instancia de jugador
+	 */
     Jugador(int numeroJugador, int cantidadDeTesoros);
     ~Jugador();
+	/*
+	 * Pre:
+	 * Pos: devuelve la cantidad de tesoros
+	 */
+	int devolverCantidadDeTesoros();
+
+	/*
+	 * Pre:
+	 * Pos: devuelve el numero de jugador
+	 */
     int devolverJugadorId();
     void obtenerCarta(Mazo* mazo);
     void jugarCarta(int posicion);
