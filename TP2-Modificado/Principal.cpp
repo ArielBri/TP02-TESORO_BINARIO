67c61ce8522a895e16fb16624e12fc18bf4bf4c8
carlicastro <105254519+carlicastro@users.noreply.github.com>
2023-11-07 18:55:07 -0300

    Create Principal.cpp

---
+++ TP2-Modificado/Principal.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Lista/Lista.h"
+#include "Pila/Pila.h"
+#include "Mazo/Mazo.h"
+#include "Tablero/Tablero.h"
+#include "TesoroBinario/TesoroBinario.h"
+#include <cstdlib>
+#include "Jugador/Jugador.h"
+#include "Coordenada/Coordenada.h"
+#include "Ficha/Ficha.h"
+#include "Interacciones.h"
+
+int main(){
+	Interacciones* interacciones = new Interacciones();
+	int cantidadJugadores = interacciones->solicitarCantidadDeJugadores();
+	int cantidadTesoros = interacciones->solicitarCantidadDeTesoros();
+	int* tamanioDeTablero = interacciones->solicitarTamanioTablero(cantidadJugadores);
+	TesoroBinario* tesoroBinario = new TesoroBinario(tamanioDeTablero[0],tamanioDeTablero[1], tamanioDeTablero[2],cantidadJugadores,cantidadTesoros);
+	return 0;
+}
