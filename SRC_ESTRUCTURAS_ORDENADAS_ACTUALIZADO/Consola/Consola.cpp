53e7108c6ec746463c5b94454424762332c6e2c5
IvanFuschetto <133823780+IvanFuschetto@users.noreply.github.com>
2023-11-08 13:29:22 -0300

    Update Consola.cpp

--- SRC_ESTRUCTURAS_ORDENADAS_ACTUALIZADO/Consola/Consola.cpp
+++ SRC_ESTRUCTURAS_ORDENADAS_ACTUALIZADO/Consola/Consola.cpp
@@ -15,3 +15,24 @@ unsigned int Consola::solicitarCantidadDeTesoros() {
     std::cin >> cantidadDeTesoros;
     return cantidadDeTesoros;
 }
+
+void Consola::menuDeOpciones() {
+    std::cout<<"MENU DE OPCIONES"<<std::endl;
+    std::cout<<"A - Sacar carta del mazo, para jugarla o guardarla: "<<std::endl;
+    std::cout<<"B - Atacar posicion del tablero con TesoroMina: "<<std::endl;
+    std::cout<<"C - Colocar espia en una posicion del tablero: "<<std::endl;
+    std::cout<<"D - Mover un tesoro de lugar: "<<std::endl;
+}
+
+void Consola::validarEleccion(char opcion){
+    if(opcion!='A' && opcion!='B' && opcion!='C' && opcion!='D'){
+        throw "Esa opcion no es valida";
+    }
+}
+
+char Consola::eleccion(char opcion){
+    std::cout<<"Ingrese una opcion"<<std::endl;
+    std::cin>> opcion;
+    validarEleccion(opcion);
+    return opcion;
+}
