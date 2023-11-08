a0365264983ac39f85de8eda8f5ee87fd1a4a354
carlicastro <105254519+carlicastro@users.noreply.github.com>
2023-11-08 05:05:01 -0300

    Update Coordenada.h

--- SRC_ESTRUCTURAS_ORDENADAS_ACTUALIZADO/Coordenada/Coordenada.h
+++ SRC_ESTRUCTURAS_ORDENADAS_ACTUALIZADO/Coordenada/Coordenada.h
@@ -10,6 +10,8 @@ private:
     int ancho;
     int alto;
     int largo;
+   	unsigned int* coordenadas; /*//<----------------------------------------------------MODIFICACION Y OBSERVACION: ESTA FUNCION DEBERIA ESTAR EN coordenadas.cpp
+
 
 public:
     /**
@@ -48,6 +50,15 @@ public:
      * pos: -
     */
     void setLargo(int largo);
+
+    /*//<----------------------------------------------------MODIFICACION Y OBSERVACION: ESTA FUNCION DEBERIA ESTAR EN coordenadas.cpp
+    *
+    * pos: si los valores no estan en rango, tira una excepcion
+    */
+    void validarCoordenadas(unsigned int ancho, unsigned int alto, unsigned int largo);
+    /*
+    */
+
     /**
     * pre: -
     * pos: -
