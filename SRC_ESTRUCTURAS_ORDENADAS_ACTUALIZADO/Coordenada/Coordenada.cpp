641a03813820e85702974af693ad741c97ddd2c7
carlicastro <105254519+carlicastro@users.noreply.github.com>
2023-11-08 05:06:44 -0300

    Update Coordenada.cpp

--- SRC_ESTRUCTURAS_ORDENADAS_ACTUALIZADO/Coordenada/Coordenada.cpp
+++ SRC_ESTRUCTURAS_ORDENADAS_ACTUALIZADO/Coordenada/Coordenada.cpp
@@ -7,7 +7,6 @@
     setLargo(largo);
 }
 
-
 int Coordenada::getAncho(){
     return this->ancho;
 }
@@ -27,6 +26,20 @@ void Coordenada::setAlto(int alto){
 void Coordenada::setLargo(int largo){
     this->largo = largo;
 }
+
+/*//<----------------------------------------------------MODIFICACION Y OBSERVACION: ESTA FUNCION DEBERIA ESTAR EN coordenadas.cpp
+*
+*/
+void Coordenada::validarCoordenadas(unsigned int anchoIngresado, unsigned int altoIngresado, unsigned int largoIngresado){
+	if (anchoIngresado < 1 || anchoIngresado > this->getAncho() || altoIngresado < 1 || altoIngresado > this->getAlto() || largoIngresado < 1 || largoIngresado > this->getLargo()) {
+		throw "Las coordenadas deben estar dentro del rango del tablero";
+	}
+	this->coordenadas[0] = anchoIngresado;
+	this->coordenadas[1] = altoIngresado;
+	this->coordenadas[2] = largoIngresado;
+}
+/*
+*/
 
 Coordenada::~Coordenada(){
 
