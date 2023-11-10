cb3b41d816587902fdda7ed050677953b4630362
JoaquinCladera <116321077+JoaquinCladera@users.noreply.github.com>
2023-11-10 14:28:44 -0300

    Add files via upload

---
+++ JetBrains/Mazo/Carta.h
@@ -0,0 +1,38 @@
+#ifndef TP_2_CARTA_H
+#define TP_2_CARTA_H
+
+
+#include "../Lista/Lista.h"
+
+enum TipoCarta {
+    Blindaje,
+    Radar,
+    PartirTesoro,
+    MostrarEspiasDeJugadorActivo,
+    ObtenerCartas4,
+    Espias2
+};
+
+
+class Carta {
+private:
+    TipoCarta tipo;
+public:
+    /*
+     * Pre:
+     * Pos:
+     */
+    explicit Carta(TipoCarta tipo);
+    /*
+     * Pre:
+     * Pos:
+     */
+    TipoCarta verCarta();
+    /*
+     * Pre:
+     * Pos:
+     */
+    ~Carta();
+};
+
+#endif
