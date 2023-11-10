2cbe14863e2bde62f48365a2bbf9cae67b666385
FelipeColodro <101566904+FelipeColodro@users.noreply.github.com>
2023-11-10 11:58:56 -0300

    Update Bitmap.h

--- JetBrains/Bitmap/Bitmap.h
+++ JetBrains/Bitmap/Bitmap.h
@@ -5,6 +5,7 @@
 #include "../Jugador/Jugador.h"
 #include "../Lista/Lista.h"
 #include "../Tablero/Casillero.h"
+#include "bitmap_image.hpp"
 
 
 #include <iostream>
@@ -21,9 +22,51 @@ public:
      */
     void mostrarTablero(Tablero* tablero, Jugador* jugador);
 
+    /*
+	 * Pre:
+	 * Pos:
+	 */
+    std::string intToString(int numero);
 
-    ~Bitmap();
+    /*
+     * Pre:
+     * Pos: Pinta la casilla del color del terreno
+     */
+    void pintarFondoCasilla(bitmap_image &image, unsigned int numeroCasillaHorizontal, unsigned int numeroCasillaVertical, unsigned int rojo, unsigned int azul, unsigned int verde,
+                            unsigned int anchoCasilla, unsigned int altoCasilla);
 
+    /*
+     * Pre:
+     * Pos: genera tres bitmap, cada uno con la vista XY, XZ y YZ del tablero
+     */
+    void crearImagenesTablero(Tablero* tablero,Jugador*jugador);
+
+    /*
+     * Pre:
+     * Pos: En funcion del objeto a pintar, llama a los metodos necesarios para pintar los objetos
+     */
+    void pintar(bitmap_image &image, unsigned int ejeVista, Casillero* casillero, unsigned int numeroCasillaHorizontalEnImagen, unsigned int numeroCasillaVerticalEnImagen,
+                unsigned int anchoCasilla, unsigned int altoCasilla,Jugador* jugador);
+
+    /*
+     * Pre:
+     * Pos: El plano tiene en sus bordes el numero de casillero, para ello se llama a dibujar numero, para las casillas, se llama al metodo pintar
+     */
+    void pintarPlano(bitmap_image &image, unsigned int ejeVista, unsigned int numeroCasillaHorizontalEnImagen, unsigned int numeroCasillaVerticalEnImagen,
+                     unsigned int cantidadCasillasHorizontalesEnImagen, unsigned int cantidadCasillasVerticalesEnImagen, unsigned int logitudEjeVista, unsigned int anchoCasilla,
+                     unsigned int altoCasilla, unsigned int i, unsigned int j, unsigned int k, std::string vista,Tablero* tablero,Jugador* jugador);
+    /*
+     * Pre:
+     * Pos: Dibuja el numero correspondiente en la casilla
+     */
+    void dibujarNumero(bitmap_image &imagen, unsigned int numeroCasillaHorizontal, unsigned int numeroCasillaVertical, unsigned int anchoCasilla, unsigned int altoCasilla, unsigned int nroCasilla);
+
+    /*
+     * Pre:
+     * Pos: dibuja los soldados, minas o vehiculos en la casilla, tambien el efecto de contaminacion
+     */
+    void dibujarObjeto(bitmap_image &imagen, unsigned int numeroCasillaHorizontal, unsigned int numeroCasillaVertical, unsigned int anchoCasilla, unsigned int altoCasilla,
+                       std::string nombreImagen);
 };
 
-#endif
+#endif /*TP_2_BITMAP_H*/
