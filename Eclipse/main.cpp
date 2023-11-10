2a5bcbe95de2bee7be9c6e8e021c5cc9f15bf0ab
carlicastro <105254519+carlicastro@users.noreply.github.com>
2023-11-10 05:33:41 -0300

    Update main.cpp
    Modificaciones comentadas

--- Eclipse/main.cpp
+++ Eclipse/main.cpp
@@ -1,39 +1,58 @@
 #include "TesoroBinario.h"
 
 /*
- * Pre:
- * Pos:
+ * Pre: los parámentros deben ser positivos
+ * Pos: Establece las dimensiones que tendra el tablero
  */
 void determinarParametros(int &ancho, int &alto, int &largo, int cantidadDeJugadores, int cantidadDeTesoros){
+	//OBSERVACION: si se ingresan 2 jugadores y 1 tesoro en tablero serian solo 2 casilleros. Se comenta una 
+	//condicion por si se quiere determinar un tablero mas grande cuando se da esa situacion.
     int cantidadDeCasillerosMinima = cantidadDeJugadores*cantidadDeTesoros;
-
-    ancho = cantidadDeCasillerosMinima/2;
-    if(cantidadDeJugadores%2!=0){
-        ancho++;
-    }
-    alto = ancho;
-    largo = cantidadDeJugadores;
+	/*if (cantidadDeCasillerosMinima==2){
+		ancho=cantidadDeCasillerosMinima*2;
+		alto = ancho;
+    	largo = cantidadDeJugadores*2;
+	}
+	else{*/
+		ancho = cantidadDeCasillerosMinima/2;
+		if(cantidadDeJugadores%2!=0){
+	        ancho++;
+	    }
+		alto = ancho;
+    	largo = cantidadDeJugadores;
+	//}
+     
 }
 
 
 /*
- * Pre:
- * Pos:
+ * Pos: Devuelve una cantidad de jugadores valida
  */
-int solicitarCantidadDeJugadores() {
+int solicitarCantidadDeJugadores(Consola* consola) {
     int cantidadDeJugadores;
     std::cout << "Ingrese la cantidad de jugadores: ";
     std::cin >> cantidadDeJugadores;
+    //<______________________________________________________________Se agrega validacion
+    while(cantidadDeJugadores<2){
+    	consola->imprimirTexto("La cantidad de jugadores debe ser mayor o igual a 2.\n");
+	    std::cout << "Ingrese la cantidad de jugadores: ";
+    	std::cin >> cantidadDeJugadores;    	
+	}
     return cantidadDeJugadores;
 }
 /*
- * Pre:
- * Pos:
+ * Pos: Devuelve una cantidad de tesoros valida.
  */
-int solicitarCantidadDeTesoros() {
+int solicitarCantidadDeTesoros(Consola* consola) {
     int cantidadDeTesoros;
     std::cout << "Ingrese la cantidad de tesoros: ";
     std::cin >> cantidadDeTesoros;
+    //<______________________________________________________________Se agrega validacion
+    while(cantidadDeTesoros<1){
+    	consola->imprimirTexto("La cantidad de tesoros debe ser mayor a 0.\n");
+	    std::cout << "Ingrese la cantidad de tesoros: ";
+    	std::cin >> cantidadDeTesoros;    	
+	}
     return cantidadDeTesoros;
 }
 
@@ -43,14 +62,15 @@ int solicitarCantidadDeTesoros() {
 int main() {
 
     TesoroBinario* tesoro;
+    Consola* consola;//<___________________________________________Se agrega para imprimir texto en las validaciones
     int cantidadJugadores, cantidadDeTesoros, alto, largo, ancho;
-    cantidadJugadores = solicitarCantidadDeJugadores();
-    cantidadDeTesoros = solicitarCantidadDeTesoros();
+    cantidadJugadores = solicitarCantidadDeJugadores(consola);
+    cantidadDeTesoros = solicitarCantidadDeTesoros(consola);
 
     determinarParametros(ancho, alto, largo, cantidadJugadores, cantidadDeTesoros);
     tesoro = new TesoroBinario(ancho, alto, largo, cantidadJugadores, cantidadDeTesoros);
     tesoro->jugar();
     delete tesoro;
-
+	delete consola;
     return 0;
 }
