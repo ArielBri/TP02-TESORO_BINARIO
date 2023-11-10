8e3cdc8f5326cbde386ff6b4775e763d8a53cb89
carlicastro <105254519+carlicastro@users.noreply.github.com>
2023-11-10 04:59:53 -0300

    Update Jugador.h
    Agrego postcondiciones

--- Eclipse/Jugador.h
+++ Eclipse/Jugador.h
@@ -17,49 +17,49 @@ private:
     EstadoJugador estado;
 
 public:
-	/*
-	 * Pre: numero de jugador y cantidad de tesoros debe ser entero mayor a 0
-	 * Pos: crea una instancia de jugador
-	 */
+    /*
+     * Pre: numero de jugador debe ser entero mayor a 0.
+     * Pos: crea una instancia de jugador.
+     */
     explicit Jugador(int numeroJugador);
-	/*
-	 * Pre:
-	 * Pos: devuelve la cantidad de tesoros
-	 */
-	int devolverCantidadDeTesoros();
-    /*
-	 * Pre:
-	 * Pos: devuelve la lista de fichas del jugador
-	 */
-    Lista<Ficha*>* obtenerFichasDelJugador();
-    /*
-	 * Pre:
-	 * Pos: devuelve la lista de cartas del jugador
-	 */
-    Lista<Carta*>* obtenerCartasDelJugador();
-	/*
-	 * Pre:
-	 * Pos: devuelve el numero de jugador
-	 */
-    int devolverJugadorId();
     /*
      * Pre:
-     * Pos:
+     * Pos: devuelve la cantidad de tesoros
+     */
+    int devolverCantidadDeTesoros();
+    /*
+     * Pre:
+     * Pos: devuelve la lista de fichas del jugador
+     */
+    Lista<Ficha*>* obtenerFichasDelJugador();
+    /*
+     * Pre:
+     * Pos: devuelve la lista de cartas del jugador
+     */
+    Lista<Carta*>* obtenerCartasDelJugador();
+    /*
+     * Pre:
+     * Pos: devuelve el numero de jugador
+     */
+    int devolverJugadorId();
+    /*
+     * Pre: El parametro recibido debe ser mayor a 0.
+     * Pos: Carga los tesoros al tablero de forma aleatoria
      */
     void inicializarTesoros(int cantidadDeTesoros);
     /*
      * Pre:
-     * Pos:
+     * Pos: Devuelve el estado del jugador.
      */
     EstadoJugador getEstadoJugador();
     /*
      * Pre:
-     * Pos:
+     * Pos: Cambia el estado del jugador.
      */
     void setEstadoJugador(EstadoJugador estadoJugador);
     /*
      * Pre:
-     * Pos:
+     * Pos: Saca una carta del mazo y la agrega a la lista de cartas del jugador.
      */
     void obtenerCarta(Mazo* mazo);
     /*
