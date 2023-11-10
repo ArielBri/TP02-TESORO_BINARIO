b07662549f4276269cf5beba051d11f16d12144d
carlicastro <105254519+carlicastro@users.noreply.github.com>
2023-11-10 05:18:26 -0300

    Update TesoroBinario.h
    Se agregaron postcondiciones

--- Eclipse/TesoroBinario.h
+++ Eclipse/TesoroBinario.h
@@ -31,43 +31,43 @@ private:
 
 public:
     /*
-     * Pre:
-     * Pos:
+     * Pre: Los parametros recibidos deben ser mayores a 0.
+     * Pos: Crea las instancias necesarias para el juego, e inicializa sus atributos.
      */
     TesoroBinario(int ancho, int alto, int largo, int cantidadDeJugadores, int cantidadDeTesoros);
     /*
-     * Pre:
+     * Pre: 
      * Pos:
      */
     void jugar();
     /*
-     * Pre:
-     * Pos:
+     * Pre: 
+     * Pos: Carga los tesoros de cada jugador al tablero de forma aleatoria
      */
     void inicializarJuego();
     /*
-     * Pre:
-     * Pos:
+     * Pre: 
+     * Pos: Devuelve un booleano que indica si hay ganador del juego.
      */
     bool hayGanador();
     /*
      * Pre:
-     * Pos:
+     * Pos: Cambia el estado del casillero a ocupado
      */
     void colocarFicha(Ficha* ficha);
     /*
     * Pre:
-    * Pos:
+    * Pos: Llama a las funciones que permiten el desarrolo del turno del jugador
     */
     void ejecutarTurno();
     /*
      * Pre:
-     * Pos:
+     * Pos: Cambia el estado de los jugadores sin tesoros para que no puedan seguir jugando
      */
     void borrarJugadoresQuePerdieron();
     /*
      * Pre: El jugador debe existir.
-     * Pos: Recorre el tablero eliminando las fichas de los jugadores.
+     * Pos: Elimina todas las fichas del jugador dueño de la lista pasada por parametro.
      */
     void limpiarCasillerosQueContenganLasFichasDelJugador(Lista<Ficha*>* fichas);
     /*
@@ -76,83 +76,83 @@ public:
      */
     void usarCarta(Carta* carta);
     /*
-     * Pre:
-     * Pos:
+     * Pre: El jugador eligio usar esta carta.
+     * Pos: Protege todos los tesoros del jugador actual de los espias y tesorominas.
      */
     void jugarBlindaje();
     /*
-     * Pre:
-     * Pos:
+     * Pre: El jugador eligio usar esta carta.
+     * Pos: Detecta 3 casilleros a la redonda de la posicion en la que se ubica, y avisa al jugador si hay tesoros, .
      */
     void jugarRadar();
     /*
-     * Pre:
-     * Pos:
+     * Pre: El jugador eligio usar esta carta.
+     * Pos: Permite al jugador insertar un tesoro extra en el tablero.
      */
     void partirTesoro();
     /*
-     * Pre:
-     * Pos:
+     * Pre: El jugador eligio usar esta carta.
+     * Pos: Permite al jugador cargar dos espias extra en su turno.
      */
     void jugarEspias2();
     /*
-     * Pre:
+     * Pre: 
      * Pos:
      */
     void jugarMostrarEspiasDeJugadorActivo();
     /*
      * Pre:
-     * Pos: Agrega una cantidad una cantidad random entre 0 y 4 de cartas a la mano del jugador
+     * Pos: Agrega una cantidad random entre 0 y 4 de cartas a la mano del jugador
      */
     void jugarObtenerCartas4();
     /*
      * Pre:
-     * Pos:
+     * Pos: Elimina la ficha de la lista del jugador.
      */
     void eliminarFichaDeLaListaDeFichasDelJugador(Ficha* ficha);
     /*
      * Pre:
-     * Pos:
+     * Pos: Actualiza la imagen del tablero con las modificaciones hechas por el jugador durante su turno.
      */
     void actualizarTablero();
     /*
      * Pre:
-     * Pos:
+     * Pos: Ejecuta la obtencion y uso de una carta por el jugador de turno.
      */
     void metodoCartas();
     /*
      * Pre:
-     * Pos:
+     * Pos: Modifica el casillero donde se coloca un TesoroMina, dependiendo del contenido previo.
      */
     void metodoTesoroMina();
     /*
      * Pre:
-     * Pos:
+     * Pos: Modifica el casillero donde se coloca un Espia, dependiendo del contenido previo.
      */
     void metodoEspias();
     /*
      * Pre:
-     * Pos:
+     * Pos: Mueve un Tesoro y modifica el casillero dependiendo del contenido previo.
      */
     void metodoMoverTesoro();
     /*
      * Pre:
-     * Pos:
+     * Pos: Muestra por consola que jugador gano la partida.
      */
     void anunciarGanador();
     /*
-     * Pre:
-     * Pos:
+     * Pre: Los parametros deben ser mayores a 0.
+     * Pos: Devuelve un booleano si en las coordenadas recibidas por parametro hay un tesoro.
      */
     bool hayTesoroEnCasillero(int X, int Y, int Z);
     /*
-     * Pre:
-     * Pos:
+     * Pre: 
+     * Pos: Coloca los tesoros en el tablero y los agrega a la lista de fichas del jugador recibido por parametro.
      */
     void inicializarTesoros(int cantidadDeTesorosPorJugador, Jugador* jugador);
     /*
      * Pre:
-     * Pos:
+     * Pos: Destruye la instancia de TesoroBinario
      */
     virtual ~TesoroBinario();
 };
