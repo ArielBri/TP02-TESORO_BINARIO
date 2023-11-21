16f62d1a1e148b426dad4f1c68488fa358f27386
carlicastro <105254519+carlicastro@users.noreply.github.com>
2023-11-21 07:30:08 +0000

    Update Mazo.cpp

--- TP2-TableroFuncional/Mazo.cpp
+++ TP2-TableroFuncional/Mazo.cpp
@@ -1,38 +1,36 @@
 #include "Mazo .h"
 
-
 Mazo::Mazo() {
     this->valor = 3;
 }
-
 
 Carta* Mazo::obtenerCarta() {
     TipoCarta tipo;
     int numero = 0;
     for(int  i = 0; i< this->valor; i++){
-        numero = (rand()%5);
+        numero = (rand()%6);
     }
-    if(this->valor=10){
+    if(this->valor==10){
         this->valor = 3;
     }
     switch (numero) {
         case 0:
-            tipo = Radar;
-            break;
-        case 1:
             tipo = Blindaje;
             break;
-        case 2:
-            tipo = Espias2;
+        case 1:
+            tipo = Radar;
             break;
-        case 3:
+        case 2:
             tipo = PartirTesoro;
             break;
-        case 4:
+        case 3:
             tipo = MostrarEspiasDeJugadorActivo;
             break;
-        case 5:
+        case 4:
             tipo =  ObtenerCartas4;
+            break;
+        case 5:
+            tipo = Espias2;
     }
     this->valor++;
     return new Carta(tipo);
