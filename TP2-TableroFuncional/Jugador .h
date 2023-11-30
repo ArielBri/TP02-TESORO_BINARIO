ba8692166418c718aa342444b863f094552cb47c
carlicastro <105254519+carlicastro@users.noreply.github.com>
2023-11-30 15:34:20 -0300

    Update Jugador .h

--- TP2-TableroFuncional/Jugador .h
+++ TP2-TableroFuncional/Jugador .h
@@ -15,33 +15,34 @@ private:
     Lista<Carta*>* cartasDelJugador;
     Lista<Ficha*>* fichas;
     EstadoJugador estado;
+
     int pierdeTurno;
 
 public:
-    /*
-     * Pre: numero de jugador debe ser entero mayor a 0
-     * Pos: crea una instancia de jugador
-     */
+	/*
+	 * Pre: numero de jugador debe ser entero mayor a 0
+	 * Pos: crea una instancia de jugador
+	 */
     explicit Jugador(int numeroJugador);
+	/*
+	 * Pre:
+	 * Pos: devuelve la cantidad de tesoros
+	 */
+	int devolverCantidadDeTesoros();
     /*
-     * Pre:
-     * Pos: devuelve la cantidad de tesoros
-     */
-    int devolverCantidadDeTesoros();
-    /*
-     * Pre:
-     * Pos: devuelve la lista de fichas del jugador
-     */
+	 * Pre:
+	 * Pos: devuelve la lista de fichas del jugador
+	 */
     Lista<Ficha*>* obtenerFichasDelJugador();
     /*
-     * Pre:
-     * Pos: devuelve la lista de cartas del jugador
-     */
+	 * Pre:
+	 * Pos: devuelve la lista de cartas del jugador
+	 */
     Lista<Carta*>* obtenerCartasDelJugador();
-    /*
-     * Pre:
-     * Pos: devuelve el numero de jugador
-     */
+	/*
+	 * Pre:
+	 * Pos: devuelve el numero de jugador
+	 */
     int devolverJugadorId();
     /*
      * Pre:
@@ -58,21 +59,25 @@ public:
      * Pos: Agrega una carta a la lista de cartas del jugador
      */
     void obtenerCarta(Mazo* mazo);
+
     /*
      * Pre:
      * Pos: coloca en 2 la cantidad de turnos que pierde el jugador
      */
     void setPierdeTurno();
-    /*
+
+    /*/
      * Pre:
      * Pos: Devuelve la cantidad de turnos perdidos
      */
     int getPierdeTurno();
+
     /*
      * Pre:
      * Pos: resta la cantidad de turnos que el jugador no puede jugar
      */
     void restarPierdeTurno();
+
     /*
      * Pre:
      * Pos: Destruye el jugador
